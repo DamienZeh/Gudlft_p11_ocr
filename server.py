4625335bdeb9544ebc3c6d1a2f6c27cd837cd54c
damie <damien.hernandez1@hotmail.fr>
2022-09-22 07:46:06 +0200

    add tests & fix bug no more than their points allowed

--- server.py
+++ server.py
@@ -64,93 +64,29 @@ def purchasePlaces():
         c for c in competitions if c["name"] == request.form["competition"]
     ][0]
     club = [c for c in clubs if c["name"] == request.form["club"]][0]
-    placesRequired = int(request.form["places"])
-    competition["numberOfPlaces"] = (
-        int(competition["numberOfPlaces"]) - placesRequired
-    )
-    flash("Great-booking complete!")
-    return render_template(
-        "welcome.html", club=club, competitions=competitions
-    )
-
-
-# TODO: Add route for points display
-
-
-@app.route("/logout")
-def logout():
-    return redirect(url_for("index"))
-import json
-from flask import Flask, render_template, request, redirect, flash, url_for
-
-
-def loadClubs():
-    with open("clubs.json") as c:
-        listOfClubs = json.load(c)["clubs"]
-        return listOfClubs
-
-
-def loadCompetitions():
-    with open("competitions.json") as comps:
-        listOfCompetitions = json.load(comps)["competitions"]
-        return listOfCompetitions
-
-
-app = Flask(__name__)
-app.secret_key = "something_special"
-
-competitions = loadCompetitions()
-clubs = loadClubs()
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/showSummary", methods=["POST"])
-def showSummary():
-    try:
-        club = [
-            club for club in clubs if club["email"] == request.form["email"]
-        ][0]
-        return render_template(
-            "welcome.html", club=club, competitions=competitions
-        )
-    except IndexError:
-        if request.form["email"] != "":
-            flash("Email not in database !")
-        else:
-            flash("You cannot leave this field empty !")
-        return render_template("index.html")
-
-
-@app.route("/book/<competition>/<club>")
-def book(competition, club):
-    foundClub = [c for c in clubs if c["name"] == club][0]
-    foundCompetition = [c for c in competitions if c["name"] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template(
-            "booking.html", club=foundClub, competition=foundCompetition
-        )
+    placesRequired = request.form["places"]
+    if placesRequired == "":
+        flash("You must type a number !")
     else:
-        flash("Something went wrong-please try again")
-        return render_template(
-            "welcome.html", club=club, competitions=competitions
-        )
-
-
-@app.route("/purchasePlaces", methods=["POST"])
-def purchasePlaces():
-    competition = [
-        c for c in competitions if c["name"] == request.form["competition"]
-    ][0]
-    club = [c for c in clubs if c["name"] == request.form["club"]][0]
-    placesRequired = int(request.form["places"])
-    competition["numberOfPlaces"] = (
-        int(competition["numberOfPlaces"]) - placesRequired
-    )
-    flash("Great-booking complete!")
+        placesRequired = int(request.form["places"])
+        if placesRequired <= 0:
+            flash("You cannot reserve 0 places or negative number !")
+        else:
+            if placesRequired <= int(
+                competition["numberOfPlaces"]
+            ) and placesRequired <= int(club["points"]):
+                competition["numberOfPlaces"] = (
+                    int(competition["numberOfPlaces"]) - placesRequired
+                )
+                flash("Great-booking complete!")
+            elif placesRequired > int(club["points"]):
+                flash("You cannot reserve more places than you have")
+            elif int(club["points"]) > int(competition["numberOfPlaces"]):
+                flash(
+                    f"Problem, the competition only has {int(competition['numberOfPlaces'])} places, you ask for {placesRequired}"
+                )
+            else:
+                flash("There was a problem, please try again")
     return render_template(
         "welcome.html", club=club, competitions=competitions
     )
--- tests/unit_tests/server_test.py
+++ tests/unit_tests/server_test.py
@@ -1,9 +1,22 @@
 import server
 
 server.clubs = [
-    {"name": "club-test1", "email": "user-exist@test.fr", "points": "1"}
+    {"name": "club-test1", "email": "user-exist@test.fr", "points": "1"},
+    {"name": "club-test2", "email": "user-exist2@test.fr", "points": "7"},
 ]
 
+server.competitions = [
+    {
+        "name": "Competition-test1",
+        "date": "2023-02-07 00:00:00",
+        "numberOfPlaces": "15",
+    },
+    {
+        "name": "Competition-test2",
+        "date": "2030-12-12 00:00:00",
+        "numberOfPlaces": "5",
+    },
+]
 """
 TESTS FOR : Error/'Entering a unknown email crashes the app' & logout :
 """
@@ -34,3 +47,97 @@ def test_mail_empty(client):
 def test_logout(client):
     response = client.get("/logout")
     assert response.status_code == 302
+
+
+"""
+TESTS FOR : Bug/'Clubs should not be able to use more than their points allowed' :
+"""
+
+
+def test_purchase_great_booking(client):
+    competitions = server.competitions[0]
+    response = client.post(
+        "/purchasePlaces",
+        data={
+            "club": "club-test1",
+            "competition": "Competition-test1",
+            "places": "1",
+        },
+    )
+    assert competitions["numberOfPlaces"] == 14
+    assert "Great-booking complete!" in str(response.data.decode())
+    assert response.status_code == 200
+
+
+def test_purchase_with_blank_field_per_competition(client):
+    response = client.post(
+        "/purchasePlaces",
+        data={
+            "club": "club-test1",
+            "competition": "Competition-test2",
+            "places": "",
+        },
+    )
+    assert "You must type a number !" in str(response.data.decode())
+    assert response.status_code == 200
+
+
+def test_purchase_too_points_entered_per_club(client):
+    response = client.post(
+        "/purchasePlaces",
+        data={
+            "club": "club-test1",
+            "competition": "Competition-test1",
+            "places": "2",
+        },
+    )
+    assert "You cannot reserve more places than you have" in str(
+        response.data.decode()
+    )
+    assert response.status_code == 200
+
+
+def test_purchase_too_points_entered_per_competition(client):
+    response = client.post(
+        "/purchasePlaces",
+        data={
+            "club": "club-test2",
+            "competition": "Competition-test2",
+            "places": "7",
+        },
+    )
+    assert str.encode(f"Number of Places: 5") in response.data
+    assert "Problem, the competition only has 5 places, you ask for 7" in str(
+        response.data.decode()
+    )
+    assert response.status_code == 200
+
+
+def test_purchase_with_zero_point_per_competition(client):
+    response = client.post(
+        "/purchasePlaces",
+        data={
+            "club": "club-test2",
+            "competition": "Competition-test2",
+            "places": "0",
+        },
+    )
+    assert "You cannot reserve 0 places or negative number !" in str(
+        response.data.decode()
+    )
+    assert response.status_code == 200
+
+
+def test_purchase_with_negative_point_per_competition(client):
+    response = client.post(
+        "/purchasePlaces",
+        data={
+            "club": "club-test2",
+            "competition": "Competition-test2",
+            "places": "-2",
+        },
+    )
+    assert "You cannot reserve 0 places or negative number !" in str(
+        response.data.decode()
+    )
+    assert response.status_code == 200
